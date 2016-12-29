52a1ce9c573114ec6f2ddfc9b642b0d282420e44
vikash verma <vkdinventor@gmail.com>
2016-12-29 19:07:48 +0530

    fullscreen button added

--- app/src/main/java/com/vikash/example/mygoogleexoplayer/MainActivity.java
+++ app/src/main/java/com/vikash/example/mygoogleexoplayer/MainActivity.java
@@ -1,14 +1,17 @@
 package com.vikash.example.mygoogleexoplayer;
 
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Surface;
-import android.view.SurfaceView;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import com.google.android.exoplayer2.DefaultLoadControl;
-import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.SimpleExoPlayer;
@@ -22,19 +25,28 @@ import com.google.android.exoplayer2.trackselection.TrackSelection;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
-import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 
 public class MainActivity extends AppCompatActivity {
+    SimpleExoPlayerView simpleExoPlayerView;
+    Button fullScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SimpleExoPlayerView simpleExoPlayerView= (SimpleExoPlayerView) findViewById(R.id.player_view);
-        SurfaceView playerSurface= (SurfaceView) findViewById(R.id.surface_view);
+        simpleExoPlayerView = (SimpleExoPlayerView) findViewById(R.id.player_view);
+        fullScreen = (Button) findViewById(R.id.download_btn);
+
+        fullScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleFullScreen();
+            }
+        });
+
 
         String userAgent = System.getProperty("http.agent");
         String url = "https://archive.org/download/Popeye_forPresident/Popeye_forPresident_512kb.mp4";
@@ -54,13 +66,10 @@ public class MainActivity extends AppCompatActivity {
         SimpleExoPlayer player =
                 ExoPlayerFactory.newSimpleInstance(this, trackSelector, loadControl);
 
-        player.setVideoSurface(playerSurface.getHolder().getSurface());
-
         simpleExoPlayerView.setPlayer(player);
 
-        DefaultBandwidthMeter defaultBandwidthMeter= new DefaultBandwidthMeter();
-        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this,
-                userAgent, defaultBandwidthMeter );
+        DefaultHttpDataSourceFactory dataSourceFactory = new DefaultHttpDataSourceFactory(
+                userAgent, null);
 
         ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
 
@@ -70,6 +79,9 @@ public class MainActivity extends AppCompatActivity {
         player.prepare(videoSource);
         player.setPlayWhenReady(true);
 
+    }
+
+    public void toggleFullScreen() {
     }
 }
 
